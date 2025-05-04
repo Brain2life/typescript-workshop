c56aee4353fdfcba56c4816df5ad7e498ff592a9
Maxat Akbanov <30049303+Brain2life@users.noreply.github.com>
2025-05-04 16:18:55 +0500

    Add freecodecamp-ts

---
+++ workshops/freecodecamp-ts/01intro/intro.js
@@ -0,0 +1,4 @@
+var user = { name: "Maxat", age: 10 };
+console.log("Maxat Akbanov");
+console.log(user.name);
+console.log(user.email);
---
+++ workshops/freecodecamp-ts/01intro/intro.ts
@@ -0,0 +1,7 @@
+let user = {name: "Maxat", age: 10}
+
+console.log("Maxat Akbanov");
+console.log(user.name);
+
+// This produces an error in code editor, but still generates a JS file
+console.log(user.email);
